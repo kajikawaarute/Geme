751942911549928743a67e0473b8ceeecc11f5fc
m y <1>
2019-06-27 13:40:42 +0900

    変更

--- GameTemplate/Game/Title.cpp
+++ GameTemplate/Game/Title.cpp
@@ -2,7 +2,7 @@
 #include "Title.h"
 #include "Game.h"
 #include "Fade.h"
-#include "Sound.h"
+
 
 Title::Title()
 {
@@ -11,9 +11,12 @@ Title::Title()
 
 Title::~Title()
 {
+
 	DeleteGO(m_spriteRender);
 	DeleteGO(m_spriteRender1);
 	DeleteGO(m_spriteRender2);
+	DeleteGO(m_Sound);
+	DeleteGO(CSkinmodel);
 
 }
 
@@ -21,12 +24,12 @@ Title::~Title()
 bool Title::Start()
 {
 
-	//文字
+	//タイトル
 	m_spriteRender2 = NewGO<prefab::CSpriteRender>(0);
 	m_spriteRender2->Init(L"sprite/title.dds", 1280.0f, 720.0f);
 	m_spriteRender2->SetPosition(m_position2);
 
-	//背景
+	//文字
 	m_spriteRender = NewGO<prefab::CSpriteRender>(0);
 	m_spriteRender->Init(L"sprite/Game.dds", 1280.0f, 720.0f);
 	m_position.y = 340;
@@ -38,8 +41,18 @@ bool Title::Start()
 	m_position1.y = -300;
 	m_spriteRender1->SetPosition(m_position1);
 
+	//Unityちゃん表示
+	CSkinmodel = NewGO<prefab::CSkinModelRender>(0);
+	CSkinmodel->Init(L"modelData/unityChan.cmo");
+	CSkinmodel->SetPosition({ 1.0f,1.0f,1.0f });
+
+	//音楽再生
+	m_Sound = NewGO<prefab::CSoundSource>(0);
+	m_Sound->Init(L"sound/Title.wav");
+	m_Sound->Play(true);
 	
 	fade = FindGO<Fade>("Fade");
+
 	return true;
 }
 
@@ -48,15 +61,22 @@ void Title::Update()
 {
 
 	//文字が動く処理
-	m_timer++;
-	if (m_timer == 1) {
-		m_position.y -= 1.0f;
+	m_timer+=GameTime().GetFrameDeltaTime();
+	if (m_timer <= 2.0f) {
+		m_position.y -= 2.0f;
 		m_spriteRender->SetPosition(m_position);
-		m_timer = 0;
+		
 	}
 
 	//ボタン押すとゲームに行く
 	if (Pad(0).IsPress(enButtonA) == true) {
+
+		//ボタンを押すと効果音再生
+		m_Sound1 = NewGO<prefab::CSoundSource>(0);
+		m_Sound1->Init(L"sound/Button.wav");
+		m_Sound1->Play(false);
+
+		//ゲームに移動
 		NewGO<Game>(0, "Game");
 		fade->StartFadeOut();
 		DeleteGO(this);
