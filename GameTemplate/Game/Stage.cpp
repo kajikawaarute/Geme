83512a55a8b8c58f38c4091accc2bd3f4d74316c
Kajikawa Arute <kbc18b07@stu.kawahara.ac.jp>
2019-07-04 10:17:58 +0900

    プレイヤーとステージが衝突するようになった

--- GameTemplate/Game/Stage.cpp
+++ GameTemplate/Game/Stage.cpp
@@ -15,6 +15,7 @@ bool Stage::Start()
 {
 	m_skinModel = NewGO<prefab::CSkinModelRender>(0);
 	m_skinModel->Init(L"modelData/stage.cmo");
+	m_staticObject.CreateMesh(CVector3::Zero, CQuaternion::Identity, CVector3::One, m_skinModel);
 	return true;
 }
 
